a8e26f0db895ea9e895253463d5bc9e1f6404dfa
Kedyn Macedonio <kedyn@csu.fullerton.edu>
2017-11-17 23:34:39 -0800

    Hexadecimal string to binary function added and made minor changes to Tool.h and getCommandType.cpp

--- Tools.h
+++ Tools.h
@@ -10,9 +10,14 @@ Here you can add tools that you think might be helpful for everyone.
 const std::string S_TO_D = "40000810";
 const std::string D_TO_S = "40000C18";
 
+const std::string LINE_176 = " -329828 3---       13.140us             -11.749s   Single          A32nD         40000C18         00000000    D32    Wr       Data ----- ----   1111111";
+const std::string LINE_195 = " -329809 3---        1.598us             -11.749s   Single          A32nD         40000810         00000000    D32    Rd       Data ----- ----   1111111";
+
+
 const int NUMBER_OF_TABS = 14;
 
 const int ADDRESS_TOKEN_NUMBER = 6;
+const int DATA_TOKEN_NUMBER    = 7;
 
 //Tokenizes data string into non-whitespace strings array
 std::string* tokenizeString(const std::string& data) {
--- getCommandType.cpp
+++ getCommandType.cpp
@@ -25,11 +25,8 @@ std::string cmdToString(const std::string& command_type) {
 }
 
 int main() {
-	std::string line_176 = " -329828 3---       13.140us             -11.749s   Single          A32nD         40000C18         00000000    D32    Wr       Data ----- ----   1111111";
-	std::string line_195 = " -329809 3---        1.598us             -11.749s   Single          A32nD         40000810         00000000    D32    Rd       Data ----- ----   1111111";
-
-	std::cout << "Line 176 is a " << cmdToString(getCommandType(line_176)) << " command\n";
-	std::cout << "Line 195 is a " << cmdToString(getCommandType(line_195)) << " command\n";
+	std::cout << "Line 176 is a " << cmdToString(getCommandType(LINE_176)) << " command\n";
+	std::cout << "Line 195 is a " << cmdToString(getCommandType(LINE_195)) << " command\n";
 
 	return 0;
 }
