0d29e4d6fb7a177f815979e55bff0ac29aa0ea30
Chibi RoggianX <digizaku@hotmail.com>
2017-11-23 07:51:08 -0800

    Add file

---
+++ readOrWrite.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include "Tools.h"
+
+std::string readOrWrite(const std::string &data) {
+	std::string *data_tokens = tokenizeString(data);
+	std::string cycle = data_tokens[CYCLE_TOKEN_NUMBER];
+
+	if (cycle == "Rd" {
+		return "Read";
+	}
+	else {
+		return "Write";
+	}
+}
+
+int main() {
+	std::cout << "Line 2: " << readOrWrite(LINE_2) << " line\n";
+	std::cout << "Line 176: " << readOrWrite(LINE_176) << " line\n";
+	std::cout << "Line 195: " << readOrWrite(Line_195) << " line\n";
+
+	return 0;
+}
